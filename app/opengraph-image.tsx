2faf0e2b4650e44a38aaea3a3f8b375217335a7d
usuijuice <jumpeisoccerguitar@gmail.com>
2026-02-22 12:55:19 +0900

    Remove unused font loading and simplify Open Graph image component

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,3 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 import { ImageResponse } from "next/og";
 
 export const alt = "Pilates Base";
@@ -11,14 +9,9 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image() {
-  const notoSansJP = await readFile(
-    join(process.cwd(), "assets/NotoSansJP-Regular.ttf"),
-  );
-
   return new ImageResponse(
     <div
       style={{
-        fontSize: 128,
         background: "#fef2f2",
         width: "100%",
         height: "100%",
@@ -26,19 +19,9 @@ export default async function Image() {
         alignItems: "center",
         justifyContent: "center",
       }}
-    >
-      Pilates Base
-    </div>,
+    />,
     {
       ...size,
-      fonts: [
-        {
-          name: "Noto Sans JP",
-          data: notoSansJP,
-          style: "normal",
-          weight: 400,
-        },
-      ],
     },
   );
 }
